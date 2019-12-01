6cd0c77c6b3fd4b09b14fc79e179271b2d96bbfe
Michiel Borkent <michielborkent@gmail.com>
2019-12-01 11:37:41 +0100

    v0.1.0

--- server/project.clj
+++ server/project.clj
@@ -1,13 +1,18 @@
-(defproject clj-kondo.lsp "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject clj-kondo.lsp "0.1.0"
+  :description "Language server for clj-kondo."
+  :url "https://github.com/borkdude/clj-kondo.lsp"
+  :license {:name "EPL-1.0"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-kondo "2019.11.23"]
                  [org.eclipse.lsp4j/org.eclipse.lsp4j "0.8.1"]]
   :main clj-kondo.lsp.main
-  :profiles {:uberjar {:global-vars {*assert* false}
+  :aot :all
+  :profiles {:uberjar {:aot :all
+                       :global-vars {*assert* false}
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"
-                                  "-Dclojure.spec.skip-macros=true"]
-                       :aot :all}})
+                                  "-Dclojure.spec.skip-macros=true"]}}
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
