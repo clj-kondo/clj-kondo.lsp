37fcdbbdc5b76ae2619d728ac26beea21bee00d6
Michiel Borkent <michielborkent@gmail.com>
2020-06-21 13:53:39 +0200

    v2020.6.21

--- script/update-project.clj
+++ script/update-project.clj
@@ -1,6 +1,6 @@
 #!/bin/sh
 #_(
-   "exec" "clojure" "-Sdeps" "{:deps,{selmer,{:mvn/version,\"1.12.17\"},clj-kondo,{:mvn/version,\"2020.06.12\"}}}" "$0"
+   "exec" "clojure" "-Sdeps" "{:deps,{selmer,{:mvn/version,\"1.12.17\"},clj-kondo,{:mvn/version,\"2020.06.21\"}}}" "$0"
    )
 
 (require '[selmer.parser :as p])
